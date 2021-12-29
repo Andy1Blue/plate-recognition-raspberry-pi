1cedf2cd57715ab20600275c1917f3c83552f263
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2021-12-29 17:00:33 +0100

    Camera fix

--- camera.js
+++ camera.js
@@ -1,11 +1,21 @@
-const Raspistill = require('node-raspistill');
-const camera = new Raspistill({
-    verticalFlip: true,
-    width: 800,
-    height: 600
-});
+const Raspistill = require('node-raspistill').Raspistill;
+// const camera = new Raspistill({
+//     verticalFlip: true,
+//     width: 800,
+//     height: 600
+// });
 
-camera.takePhoto('test.jpg');
+// camera.takePhoto('test.jpg');
+
+const raspistill = new Raspistill();
+
+raspistill.takePhoto()
+    .then((photo) => {
+        console.log('took photo', photo);
+    })
+    .catch((error) => {
+        console.error('something bad happened', error);
+    });
 
 // const camera = new Raspistill();
 
