72f1827798545bdbd3f221cc6cc4399fac20eb57
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2022-01-01 20:03:26 +0100

    Remove unessesary files

--- modules/button.js
+++ modules/button.js
@@ -1,15 +1,23 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(16, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
-var sleep = require("./node_modules/sleep/");
+var sleep = require('sleep');
 
-pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-  if (err) { //if an error
+class Button {
+  constructor() {}
+
+  
+}
+
+pushButton.watch(function (err, value) {
+  //Watch for hardware interrupts on pushButton GPIO, specify callback function
+  if (err) {
+    //if an error
     console.error('There was an error', err); //output error message to console
-  return;
+    return;
   }
 
-  console.log('Button pushed')
+  console.log('Button pushed');
   LED.writeSync(1); //turn LED on or off depending on the button state (0 or 1)
   sleep.usleep(1000000);
   LED.writeSync(0);
@@ -19,10 +27,13 @@ pushButton.watch(function (err, value) { //Watch for hardware interrupts on push
   LED.writeSync(0);
 });
 
-function unexportOnClose() { //function to run when exiting program
+function unexportOnClose() {
+  //function to run when exiting program
   LED.writeSync(0); // Turn LED off
   LED.unexport(); // Unexport LED GPIO to free resources
   pushButton.unexport(); // Unexport Button GPIO to free resources
-};
+}
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+
+module.exports = {};
