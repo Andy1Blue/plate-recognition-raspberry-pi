5352360f8a736cfb6db45fe7e3dbc092f577645f
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2022-01-01 20:02:55 +0100

    Separate camera module

---
+++ modules/camera.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+const DIR = '../photos/';
+const EXTENSION = '.jpg';
+
+class Camera {
+  constructor() {}
+
+  takePhoto(fileName = Date.now()) {
+    new Promise((resolve, reject) => {
+      exec(`libcamera-jpeg -o ${DIR}${fileName}${EXTENSION}`, (error, stdout, stderr) => {
+        if (error) {
+          reject(stderr);
+        }
+
+        resolve(`${stdout}${stderr}`);
+      });
+    });
+  }
+}
+
+module.exports = Camera;
