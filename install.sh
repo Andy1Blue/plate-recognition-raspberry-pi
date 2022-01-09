ea2ff93afcb32513c34cc39f1320f9f022a7bdfe
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2022-01-09 14:50:23 +0100

    Update install sh file

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-npm install node-gyp && npm install epoll && curl -sL https://github.com/Seeed-Studio/grove.py/raw/master/install.sh | sudo bash -s - && npm install --build-from-source && sh install-alpr.sh
+sudo apt update && sudo apt upgrade && npm install node-gyp && npm install epoll && curl -sL https://github.com/Seeed-Studio/grove.py/raw/master/install.sh | sudo bash -s - && npm install --build-from-source && sh install-alpr.sh
